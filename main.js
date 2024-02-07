4ec599caadbddebc9c9c21254de9498c6546cb4d
Rafal Lisewski <rafal.lisewski@dreamhost.com>
2024-02-07 10:44:25 +0100

    The Great Return

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('siema');
+const firstName = 'Rafał';
+const age = 38;
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+console.log(`Witaj przybyszu!`);
+
+const about = document.querySelector('.about__paragraph--js');
+console.log(about.innerHTML);
+
+about.innerHTML = "Co tu sie wyczynia?";
+
+console.log(about.innerHTML);
+
+function hey(age, firstName) {
+  console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+}
+
+hey(age, firstName);
+
+function createContent(querySelector,content) {
+    const element = document.querySelector(querySelector);
+    element.innerHTML = content;
+}
+createContent('.about__paragraph--js','Co tu sie wyczynia teraz?');
+// 14323372
